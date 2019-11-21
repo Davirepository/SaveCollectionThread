46fdade6fadeac0ff5e7264e815be380cc48beef
BolotaevDavid <bolotaev-david@mail.ru>
2019-11-21 15:31:42 +0300

    add count value

--- SaveThreadCollection/SaveThreadCollection/Collection.swift
+++ SaveThreadCollection/SaveThreadCollection/Collection.swift
@@ -11,6 +11,7 @@ import Foundation
 class Collection {
     
     private var collection = [0]
+    private var count = 0
     
     private let concurrentQueue = DispatchQueue(label: "com.collection.barrier",
                                                 qos: .unspecified,
@@ -22,6 +23,8 @@ class Collection {
         concurrentQueue.async(flags: .barrier) {
             let randomInt = Int.random(in: 1...200)
             self.collection.append(randomInt)
+            self.count = self.collection.count
+            print(self.count)
             print("Barrier did set \(randomInt) \n\(self.collection)")
         }
     }
@@ -31,6 +34,8 @@ class Collection {
             let index = self.collection.count
             let randomValueToDelete = Int.random(in: 0..<index)
             let deletedValue = self.collection.remove(at: randomValueToDelete)
+            self.count = self.collection.count
+            print(self.count)
             print("Count of numbers in array = \(index). Index to delete = \(randomValueToDelete)")
             print("deleted value = \(deletedValue)")
         }
